6fa81004f819b213f2f59856079b2ca6fd6b9bfb
Moein <m.vafapour98@gmail.com>
2022-03-10 00:33:58 +0330

    Automapper Profile Added

---
+++ HiringManagementSystem/Profiles/AutoMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HiringManagementSystem.Models.DomainModels.Aggregates;
+using HiringManagementSystem.Models.ViewModels.Dtos;
+
+namespace HiringManagementSystem.Profiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        #region [- Ctor -]
+        public AutoMapperProfile()
+        {
+            CreateMap<PersonAggregate, PersonDtoViewModel>().ReverseMap();
+            CreateMap<TagAggregate, TagDtoViewModel>().ReverseMap();
+        } 
+        #endregion
+    }
+}
